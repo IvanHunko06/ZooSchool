a4811633b3427cab6937585cd858172e1ed96514
NoFruatGames <gamercombat57@gmail.com>
2024-05-17 10:34:35 +0300

    init

---
+++ SchoolServer/Controllers/AccountController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SchoolServer.Models;
+namespace SchoolServer.Controllers
+{
+    [Route("api/acount")]
+    public class AccountController : ControllerBase
+    {
+        private readonly ILogger<AccountController> _logger;
+
+        public AccountController(ILogger<AccountController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost("create_account")]
+        public async Task<IActionResult> CreateAccount([FromBody] CreateAccountModel account)
+        {
+            return Ok(new { code = 0 });
+        }
+    }
+}
---
+++ SchoolServer/Models/CreateAccountModel.cs
@@ -0,0 +1,8 @@
+﻿namespace SchoolServer.Models
+{
+    public class CreateAccountModel
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+}
